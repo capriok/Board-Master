d5803051fed8249ed0198101951a0c14a6f13124
Kyle Caprio <capriok7@yahoo.com>
2020-12-01 17:50:37 -0700

    touches

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,13 +10,11 @@ const app = express()
 const server = http.createServer(app)
 
 const port = process.env.PORT || 9000
+const origins = ['http://localhost:3000', 'https://board-master.netlify.app']
 
 app.use(express.json())
 
-app.use(cors(corsOptions(['http://localhost:3000', 'https://board-master.netlify.app'])), corsMiddleware)
-
-
-console.log(process.env.NODE_ENV);
+app.use(cors(corsOptions(origins)), corsMiddleware)
 
 let netlifyProdEndpoint = process.env.NODE_ENV === 'development' ? '' : '/.netlify/functions/server'
 
@@ -24,7 +22,11 @@ app.use(`${netlifyProdEndpoint}/io`, socket.router)
 
 server.listen(port, () => console.log(`Server running on port ${port}`))
 
-socket.initialize(io(server, { path: `${netlifyProdEndpoint}/socket`, serveClient: false }))
+socket.initialize(io(server, {
+	path: `${netlifyProdEndpoint}/socket`,
+	origins: origins,
+	serveClient: false
+}))
 
 module.exports = app
 module.exports.handler = serverless(app)
